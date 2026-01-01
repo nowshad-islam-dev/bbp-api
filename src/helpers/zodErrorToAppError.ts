5680bc8e3361b6c590f4cbd9dfc4226c900844b1
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2026-01-01 15:22:19 +0600

    update zodToAppError to use flattened field errors and enhance AppError interface for better error reporting

--- src/helpers/zodErrorToAppError.ts
+++ src/helpers/zodErrorToAppError.ts
@@ -1,15 +1,16 @@
-import { ZodError } from 'zod';
+import * as z from 'zod';
 import { AppError } from '../middlewares/error';
 
-export function zodToAppError(err: ZodError): AppError {
-    const msg = err.issues
-        .map((e) => `${e.path.join('.')}: ${e.message}`)
-        .join(', ');
+export function zodToAppError(err: z.ZodError): AppError {
+    const fieldErrors = err.flatten().fieldErrors;
+    console.log(fieldErrors);
 
-    const appErr = new Error(msg) as AppError;
+    const appErr = new Error('Validation failed') as AppError;
+
     appErr.status = 'fail';
     appErr.statusCode = 400;
     appErr.isOperational = true;
+    appErr.errors = fieldErrors;
 
     return appErr;
 }
