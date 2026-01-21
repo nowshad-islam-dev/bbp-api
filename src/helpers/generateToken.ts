0d8e0625daa3a8b0335ea504b9551de21f6a28ad
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2026-01-22 00:13:03 +0600

    refactor token generation to use JwtPayload for improved type safety

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -2,20 +2,24 @@ import crypto from 'node:crypto';
 import jwt from 'jsonwebtoken';
 import ENV from '@/config/env';
 import redisClient from '@/redis';
-import { AuthPayload } from '@/types/auth';
+import { JwtPayload } from '@/middlewares/authenticate';
 
 export const hashToken = (token: string) =>
     crypto.createHash('sha256').update(token).digest('hex');
 
-export const generateAccessToken = (user: AuthPayload) => {
-    return jwt.sign({ id: user.id, role: user.role }, ENV.ACCESS_TOKEN_SECRET, {
-        expiresIn: `${ENV.ACCESS_TOKEN_EXPIRY}s`,
-    });
+export const generateAccessToken = (user: JwtPayload) => {
+    return jwt.sign(
+        { userId: user.userId, role: user.role },
+        ENV.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: `${ENV.ACCESS_TOKEN_EXPIRY}s`,
+        },
+    );
 };
 
-export const generateRefreshToken = async (user: AuthPayload) => {
+export const generateRefreshToken = async (user: JwtPayload) => {
     const token = jwt.sign(
-        { id: user.id, role: user.role },
+        { userId: user.userId, role: user.role },
         ENV.REFRESH_TOKEN_SECRET,
         {
             expiresIn: `${ENV.REFRESH_TOKEN_EXPIRY}s`,
@@ -24,7 +28,7 @@ export const generateRefreshToken = async (user: AuthPayload) => {
 
     const tokenHash = hashToken(token);
 
-    await redisClient.set(`refresh:${tokenHash}`, String(user.id), {
+    await redisClient.set(`refresh:${tokenHash}`, String(user.userId), {
         expiration: {
             type: 'EX',
             value: ENV.REFRESH_TOKEN_EXPIRY,
