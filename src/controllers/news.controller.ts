2f34427eca63bd3faf18a1d8c037bcf5f99d75e8
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2026-01-29 00:06:39 +0600

    refactor news creation: simplify image handling in create method

--- src/controllers/news.controller.ts
+++ src/controllers/news.controller.ts
@@ -18,10 +18,7 @@ export class NewsController extends BaseController {
         this.handleRequest(req, res, next, async () => {
             const { title, text } = req.body as NewsBody;
             const img = res.locals.fileUrl as string | undefined;
-
-            return img
-                ? await this.newsService.create(title, text, img)
-                : await this.newsService.create(title, text);
+            return await this.newsService.create(title, text, img);
         });
     };
 
